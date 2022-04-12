fc92a25d7916cabc91cbe084a00065114989e867
猴子 <70706886+a3510377@users.noreply.github.com>
2022-04-12 17:27:36 +0800

    edit

--- main.py
+++ main.py
@@ -1,71 +1,70 @@
 import json
 import os
-
 import requests
-from discord.ext import commands, tasks
 from dotenv import load_dotenv
+from discord.ext import commands, tasks
 
-import discord
-from function import *
+from typing import Optional, List
+
 
 load_dotenv()
-bot = commands.Bot(command_prefix="!")
-data = sets(
-    os.getenv("token"), APIToken=os.getenv("APIToken"))
+
+API_URLS = {
+    "feel": "https://opendata.cwb.gov.tw/api/v1/rest/datastore/E-A0015-001?Authorization={token}&limit=1&areaName=",
+    "smallArea": "https://opendata.cwb.gov.tw/api/v1/rest/datastore/E-A0016-001?Authorization={token}&limit=1&areaName="
+}
 
 
-def setup():
-    try:
-        open(data.checkFile)
-    except:
-        with open(data.checkFile, "w") as outfile:
-            json.dump({}, outfile, ensure_ascii=False, indent=4)
-            print("建立 check.json 完成")
+class Bot(commands.Bot):
+    def __init__(
+        self,
+        *,
+        channels: Optional[List[str]] = None,
+        tags: Optional[List[str]] = None,
+        checkFile: Optional[str] = None,
+        APIToken: Optional[str] = None,
+        command_prefix: Optional[str] = None,
+    ):
+        super().__init__(
+            command_prefix=command_prefix or os.getenv("PREFIX", "!"),
+            help_command=None,
+        )
+
+        self.tags: List[str] = \
+            tags or list(map(str, os.getenv("TAGS", "").split())) or []
+        self.checkFile: str = \
+            checkFile or os.getenv("CHECK_FILE", "check.json")
+        self.APIToken: str = APIToken or os.getenv("APIToken")
+        self.channels: List[str] = channels or []
+
+        if not self.APIToken:
+            raise Exception(
+                "TOKEN 錯誤或未設置，請至 https://opendata.cwb.gov.tw/userLogin 獲取中央氣象局TOKEN並放置於 .env 檔案中"
+            )
+
+        try:
+            open(self.checkFile)
+        except FileNotFoundError:
+            with open(self.checkFile, "w", encoding="utf-8") as file:
+                json.dump({}, file, ensure_ascii=False)
+                print(f"建立 {self.checkFile} 完成")
+
+    async def on_ready(self):
+        print("-" * 15)
+        print(self.user.name)
+        print(self.user.id)
+        print(self.user)
+        print("-" * 15)
+        self.earthquake.start()
+
+    @tasks.loop(minutes=1)
+    async def earthquake(self):
+        for url in API_URLS:
+            r = requests.get(url).json()
+            earthquake = r["records"]["earthquake"]
+            if r["success"] != "true" or not earthquake:
+                continue
 
 
-@bot.event
-async def on_ready():
-    print("-"*15)
-    print(bot.user.name)
-    print(bot.user.id)
-    print(bot.user)
-    print("-"*15)
-    setup()
-    if data.APIToken:
-        earthquake.start()
-        print("地震報告啟動")
-    else:
-        print("請至 https://opendata.cwb.gov.tw/userLogin 獲取中央氣象局TOKEN並放置於 .env 檔案中")
-
-
-@tasks.loop(seconds=10)
-async def earthquake():
-    # 大型地震
-    API = f"https://opendata.cwb.gov.tw/api/v1/rest/datastore/E-A0015-001?Authorization={data.APIToken}&format=JSON&areaName="
-    # 小型地震
-    API2 = f"https://opendata.cwb.gov.tw/api/v1/rest/datastore/E-A0016-001?Authorization={data.APIToken}&format=JSON"
-
-    b = requests.get(API).json()
-    s = requests.get(API2).json()
-    _API = b["records"]["earthquake"][0]["earthquakeInfo"]["originTime"]
-    _API2 = s["records"]["earthquake"][0]["earthquakeInfo"]["originTime"]
-
-    async def goTo(how, now):
-        for ch in data.channels:
-            await sosIn(bot.get_channel(ch), ({API: b, API2: s}[how]), data)
-        with open(data.checkFile, 'w') as outfile:
-            json.dump(now, outfile, ensure_ascii=False, indent=4)
-
-    with open(data.checkFile, "r") as file:
-        file = json.load(file)
-    for i in [API, API2]:
-        if not file.get(i):
-            file[i] = ""
-    if file[API] != _API:
-        file[API] = _API
-        await goTo(API, file)
-    if file[API2] != _API2:
-        file[API2] = _API2
-        await goTo(API2, file)
-
-bot.run(data.token)
+bot = Bot()
+bot.run(os.getenv("TOKEN"))
